3187e6227bec27274ed8d21e4e80902e3c610615
Zwe <zewlinnmg@gmail.com>
2026-02-23 23:15:17 +0700

    Fix layout

--- src/app/(dashboard)/layout.tsx
+++ src/app/(dashboard)/layout.tsx
@@ -7,7 +7,7 @@ export default function DashboardLayout({
   children: React.ReactNode;
 }) {
   return (
-    <SidebarProvider>
+    <SidebarProvider className="flex flex-col sm:flex-row">
       <SideBar />
       <SidebarTrigger className="p-5" />
       <main className="w-full">{children}</main>
